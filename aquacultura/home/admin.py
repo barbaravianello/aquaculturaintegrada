01b8bc671c864212ce8d73a02dbb3741749a4efe
barbaravianello.github <barbaravianello@ejectufrn.com.br>
2017-10-09 01:21:08 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	aquacultura/db.sqlite3
    #	aquacultura/home/__pycache__/admin.cpython-36.pyc
    #	aquacultura/home/__pycache__/models.cpython-36.pyc
    #	aquacultura/home/__pycache__/urls.cpython-36.pyc
    #	aquacultura/home/__pycache__/views.cpython-36.pyc
    #	aquacultura/home/admin.py
    #	aquacultura/home/models.py
    #	aquacultura/home/static/css/style.css
    #	aquacultura/home/templates/contact.html
    #	aquacultura/home/templates/index.html
    #	aquacultura/home/templates/services.html
    #	aquacultura/home/urls.py
    #	aquacultura/home/views.py

--- aquacultura/home/admin.py
+++ aquacultura/home/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Team
-=======
-from .models import Team, Service, Portifolio
-
->>>>>>> origin/master
 
 class TeamAdmin(admin.ModelAdmin):
 
@@ -12,26 +7,5 @@ class TeamAdmin(admin.ModelAdmin):
 	search_fields = ['name', 'slug']
 	prepopulated_fields = {'slug': ['name']}
 
-<<<<<<< HEAD
 admin.site.register(Team, TeamAdmin)
-=======
-
-class ServiceAdmin(admin.ModelAdmin):
-
-	list_display = ['name', 'slug']
-	search_fields = ['name', 'slug']
-	prepopulated_fields = {'slug': ['name']}
-
-
-class PortifolioAdmin(admin.ModelAdmin):
-	
-	list_display = ['name', 'slug']
-	search_fields = ['name', 'slug']
-	prepopulated_fields = {'slug': ['name']}
-
-
-admin.site.register(Team, TeamAdmin)
-admin.site.register(Service, ServiceAdmin)
-admin.site.register(Portifolio, PortifolioAdmin)
->>>>>>> origin/master
 
