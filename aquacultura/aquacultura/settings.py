0d708f5ff1929807335a77d177400bcec5b72312
lmedson <medson095@gmail.com>
2017-11-01 14:04:18 -0300

    #0.x: Modificações no banco

--- aquacultura/aquacultura/settings.py
+++ aquacultura/aquacultura/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'aquacultura.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aquacultura_db',
+        'USER': 'aquacultura_user',
+        'PASSWORD': 'aqua#2017',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
