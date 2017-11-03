4f499a63b34a9210b3c2acf83e58f53221f4bca2
Larisse Évelyn <larissebcarvalho@gmail.com>
2017-11-02 23:07:51 -0300

    #17: Fixando bugs nas imagens do Quem Somos.

--- aquacultura/aquacultura/settings.py
+++ aquacultura/aquacultura/settings.py
@@ -78,13 +78,14 @@ WSGI_APPLICATION = 'aquacultura.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -96,6 +97,7 @@ DATABASES = {
         
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -213,12 +215,12 @@ CKEDITOR_CONFIGS = {
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Contato <anizio@aquaculturaintegrada.com.br>'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'email@gmail.com'
 EMAIL_HOST_PASSWORD = 'senha'
 EMAIL_PORT = 587
 
-CONTACT_EMAIL = 'contato@aquacultura.com'
+CONTACT_EMAIL = 'anizio@aquaculturaintegrada.com.br'
